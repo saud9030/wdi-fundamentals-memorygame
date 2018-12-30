e921dfd689b8106d06309508e11f4e913400d31a
SAUD <saud0@SAUDs-MacBook-Pro.local>
2018-12-30 05:39:18 +0300

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+var card01 = "Queen";
+var card02 = "King" ;
+var card03 = "Prince";
+var card04 = "Princess";
+console.log("User flipped " + card01);
+console.log("User flipped " + card02);
