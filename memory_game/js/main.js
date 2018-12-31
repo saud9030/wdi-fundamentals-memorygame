e9a515d3e8b89413f79688d4921f70035f1dc355
SAUD <saud0@SAUDs-MacBook-Pro.local>
2018-12-31 13:06:36 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,23 @@
-console.log("Up and running!");
-var card01 = "Queen";
-var card02 = "King" ;
-var card03 = "Prince";
-var card04 = "Princess";
+
+
+
+var cards = ['Queen','Queen','King','King'];
+var cardsInPlay = [];
+var card01 = cards[0];
+cardsInPlay.push(card01);
 console.log("User flipped " + card01);
+var card02 = cards[2];
+cardsInPlay.push(card02);
 console.log("User flipped " + card02);
+
+if (cardsInPlay.length === 2)
+{
+	if (cardsInPlay[0] === cardsInPlay[1])
+	{
+		alert("You found a match!!");
+	}
+	else
+	{
+		alert("Sorry, try again");
+	}
+}
