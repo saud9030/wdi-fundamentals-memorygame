bb57531a812cffc8721d465367714539ff4fec9c
SAUD <saud0@SAUDs-MacBook-Pro.local>
2019-01-04 17:12:58 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,7 +23,8 @@ var cards = [{
 			 }
 			];
 var cardsInPlay = [];
-var checkForMatch = function(){
+var checkForMatch = function()
+{
 	if (cardsInPlay.length === 2)
 	{
 		if (cardsInPlay[0] === cardsInPlay[1])
@@ -36,15 +37,91 @@ var checkForMatch = function(){
 		}
 	}
 }
-var flipCard = function(cardId)
+var flipCard = function()
 {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute('src',cards[cardId].cardImage);
 	checkForMatch();
 }
-flipCard(0);
-flipCard(2);
+
+
+var createBoard = function()
+{
+	for (var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+createBoard();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
